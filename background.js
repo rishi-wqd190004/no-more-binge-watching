d901ec6574cb3f16239460a807a4fe5de3b25935
wqd190004-rishi <wqd190004@siswa.um.edu.my>
2025-05-01 10:49:54 +0100

    slight change

--- background.js
+++ background.js
@@ -1,10 +1,10 @@
-chrome.runtime.onMessage.addListener((message, sender) => {
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.type === "updateCount") {
       chrome.storage.local.set({ videoCount: message.count });
     }
   
     if (message.type === "closeTabRequest") {
-      if (sender.tab?.id) {
+      if (message.type === "closeTabRequest" && sender.tab && sender.tab.id) {
         chrome.tabs.remove(sender.tab.id);
       } else {
         chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
